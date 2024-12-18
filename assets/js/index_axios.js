f21d5fd851ceb544e8d8e19ac8d07e1584213de8
Hyorim K <37091594+daydeiday@users.noreply.github.com>
2024-12-18 18:08:24 +0900

    [Feat] response µ¥ÀÌÅÍ ÆĽÌÇؼ­ ¹®¼­ Á¶ÀÛÇϿ© ³»¿ë ä¿ì±â

---
+++ assets/js/index_axios.js
@@ -0,0 +1,11 @@
+const getBooksInfor = async () => {
+    try{
+        const response = await axios.get("https://33b74478-d8fd-4866-bc92-abc827b6c0fe.mock.pstmn.io/api/test2");
+        console.log(response);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+console.log(getBooksInfo());
