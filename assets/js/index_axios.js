1d4abf6c2ea1f3ae6a76617cb7cebbdd94d6751e
Hyorim K <37091594+daydeiday@users.noreply.github.com>
2024-12-19 09:13:07 +0900

    [Fix] 식별자 오타 수정

--- assets/js/index_axios.js
+++ assets/js/index_axios.js
@@ -1,4 +1,4 @@
-const getBooksInfor = async () => {
+const getBooksInfo = async () => {
     try{
         const response = await axios.get("https://33b74478-d8fd-4866-bc92-abc827b6c0fe.mock.pstmn.io/api/test2");
         console.log(response);
